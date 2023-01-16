9c9f0b5b58d491298dc29c302cff7f7613a5294b
chaehwi <chaehwi.h@gmail.com>
2023-01-16 16:29:53 +0900

    오븐시계 주석 추가 / 주사위 문제 시작

---
+++ src/level_2/Baekjoon_2480_주사위세개.java
@@ -0,0 +1,13 @@
+package level_2;
+
+import java.util.Scanner;
+
+public class Baekjoon_2480_주사위세개 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		
+	}
+
+}
--- src/level_2/Baekjoon_2525_오븐시계.java
+++ src/level_2/Baekjoon_2525_오븐시계.java
@@ -7,12 +7,12 @@ public class Baekjoon_2525_오븐시계 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		int A = scan.nextInt();
-		int B = scan.nextInt();
-		int C = scan.nextInt();
+		int A = scan.nextInt(); // 시
+		int B = scan.nextInt(); // 분
+		int C = scan.nextInt(); // 더할 분
 		
-		int min = A * 60 + B;
-		min = min + C;
+		int min = A * 60 + B; // A,B (현재 시, 분을 분으로 변환)
+		min = min + C; // 분으로 변환한 값에 분 더하기
 
 		int hour = (min / 60) % 24;
 		int minute = min % 60;
