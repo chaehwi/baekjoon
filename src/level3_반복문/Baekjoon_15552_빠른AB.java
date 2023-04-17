0406fda230f7cf5a6089555f8ac04fea3a99a1ad
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-04-17 14:33:40 +0900

    15552 빠른 A + B

---
+++ src/level3_반복문/Baekjoon_15552_빠른AB.java
@@ -0,0 +1,29 @@
+package level3_반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Baekjoon_15552_빠른AB {
+
+	public static void main(String[] args)throws IOException {
+		
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			bw.write((Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()))+ "\n");
+		}
+		
+		bw.close();
+		
+	}
+
+}
