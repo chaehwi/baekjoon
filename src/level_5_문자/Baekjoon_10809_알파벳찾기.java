8802237b0cb81ac07d0ed84fe7391cb6bff31985
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-04-28 23:52:37 +0900

    문자열 알파벳찾기

--- src/level_5_문자/Baekjoon_10809_알파벳찾기.java
+++ src/level_5_문자/Baekjoon_10809_알파벳찾기.java
@@ -9,9 +9,29 @@ public class Baekjoon_10809_알파벳찾기 {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		String S = scan.next();
+		String S = scan.nextLine();
+		
+		int[] apb = new int[26];
 		
 		
+		for(int i = 0; i < apb.length; i++ ) {
+			
+			apb[i] = -1;
+			
+		}
+		
+		for(int i =0; i < S.length(); i++) {
+			
+			char s = S.charAt(i);
+			
+			if(apb[s - 'a'] == -1) {
+				apb[s - 'a'] = i;
+			}
+		}
+		
+		for(int val : apb) {
+			System.out.print(val + " ");
+		}
 		
 		
 	}
