245a31ada1d0f44c7cfb06f1d89f99caa4417087
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-05-05 23:19:11 +0900

    심화 별찍기

--- src/level_6_심화1/Baekjoon_2444_별찍기7.java
+++ src/level_6_심화1/Baekjoon_2444_별찍기7.java
@@ -17,17 +17,22 @@ public class Baekjoon_2444_별찍기7 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		
-		
-		for(int i = 1; i <= N; i++) {
+		for(int i = 0; i < N; i++) {
 			
-			for(int j = 1; j <= i; j++) {
-				bw.write("*");
-			}
-			bw.write("\n");
+			for(int j = 0; j < N-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
 		}
 		
-		bw.close();
+		for(int i = N-1; i >= 0 ; i--) {
+			for(int j = 0; j < N-i; j++)
+				System.out.print(" ");
+			for(int j = 0; j < i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
 		
 	}
 
