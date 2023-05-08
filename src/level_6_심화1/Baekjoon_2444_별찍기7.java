bb63031ff5456d4326564274d7c453c2bc337994
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-05-08 21:57:26 +0900

    심화 별찍기 수정

--- src/level_6_심화1/Baekjoon_2444_별찍기7.java
+++ src/level_6_심화1/Baekjoon_2444_별찍기7.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Baekjoon_2444_별찍기7 {
@@ -12,25 +13,30 @@ public class Baekjoon_2444_별찍기7 {
 	public static void main(String[] args)throws IOException {
 		
 	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < N; i++) {
+		for(int i = 1; i <= N; i++) {
 			
-			for(int j = 0; j < N-i; j++)
+			for(int j = 0; j < N-i; j++) {
 				System.out.print(" ");
-			for(int j = 0; j < i*2-1; j++)
+			}
+			
+			for(int j = 0; j < i*2-1; j++) {
 				System.out.print("*");
+			}
 			System.out.println();
 		}
 		
 		for(int i = N-1; i >= 0 ; i--) {
-			for(int j = 0; j < N-i; j++)
+			
+			for(int j = 0; j < N-i; j++) {
 				System.out.print(" ");
-			for(int j = 0; j < i*2-1; j++)
+			}
+			
+			for(int j = 0; j < i*2-1; j++) {
 				System.out.print("*");
+			}
 			System.out.println();
 		}
 		
