70c3037104e1140f8029df858699ecbd18c738ed
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-05-09 23:54:49 +0900

    심화 문제 연습

---
+++ src/level_6_심화1/Baekjoon_10812_바구니순서바꾸기.java
@@ -0,0 +1,45 @@
+package level_6_심화1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10812_바구니순서바꾸기 {
+
+	public static void main(String[] args)throws IOException {
+		
+	
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			
+			for(int j = 0; j < N-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j < i*2-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = N-1; i >= 0 ; i--) {
+			
+			for(int j = 0; j < N-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j < i*2-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
