65b9d2cd4037422823e784d3d3640c3cb1638e20
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-05-18 23:47:25 +0900

    바구니

--- src/level_6_심화1/Baekjoon_10812_바구니순서바꾸기.java
+++ src/level_6_심화1/Baekjoon_10812_바구니순서바꾸기.java
@@ -62,11 +62,59 @@ public class Baekjoon_10812_바구니순서바꾸기 {
             }
 		}
 		
+		// 결과 출력
 		for(int e = 0; e<Narr.length; e++){
             System.out.print(Narr[e]+" ");
         }
 		
-		
+//		 BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+//	     BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//	        String str = bfr.readLine();
+//	        StringTokenizer st = new StringTokenizer(str, " ");
+//	        int N = Integer.parseInt(st.nextToken());
+//	        int M = Integer.parseInt(st.nextToken());
+//	        int basket[] = new int[N + 1];
+//	        int newBasket[] = new int[N + 1];
+//
+//	        for (int s = 1; s <= N; s++) {
+//	            basket[s] = s;
+//	            newBasket[s] = s;
+//	        }
+//
+//	        String method;
+//	        StringTokenizer stt;
+//	        int i, j, k;
+//
+//	        for (int s = 0; s < M; s++) {
+//	            method = bfr.readLine();
+//	            stt = new StringTokenizer(method);
+//	            i = Integer.parseInt(stt.nextToken());
+//	            j = Integer.parseInt(stt.nextToken());
+//	            k = Integer.parseInt(stt.nextToken());
+//	            int ii = i;
+//
+//	            for (int g = 0; g < j - i + 1; g++) {
+//	                if (k + g <= j) {
+//	                    newBasket[g + i] = basket[k + g];
+//	                } else {
+//	                    newBasket[g + i] = basket[ii];
+//	                    ii++;
+//	                }
+//	            }
+//
+//	            for (int g = 1; g <= N; g++) {
+//	                basket[g] = newBasket[g];
+//	            }
+//	        }
+//
+//	        for (int s = 1; s <= N; s++) {
+//	            bfw.write(newBasket[s] + " ");
+//	        }
+//
+//	        bfr.close();
+//	        bfw.flush();
+//	        bfw.close();
 	}
 
 }
